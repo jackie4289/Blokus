3f726ccdc74a810560ed0c9380f4fdd7f8d25913
jackie4289 <jackie._lin@hotmail.com>
2022-05-26 09:27:11 -0400

    Create Blockus.java

---
+++ Blockus.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Blockus{
+	//Properties
+	
+	//Methods
+	
+	//Constuctor 
+	public Blockus(){
+		
+		
+	}
+	
+	//Main Program
+	public static void main(String[] args){
+		Blockus Block = new Blockus();
+	}  
+}
+	
+
