d3b2da00da9886f821fb30a6c26489f466d647e5
Lucas-M88 <lucas.moran8@gmail.com>
2022-05-27 11:19:58 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -1,13 +1,9 @@
 public class Block{
 	//Properties
-<<<<<<< HEAD
 	int intBoard[][] = new int[20][20];
-
-=======
-	String strRow[];
-	String strMap[][];
-	strMap = new String[20][20];
->>>>>>> 8da9ab08206b27f87a2adf9fb88a51ff03fa5b08
+		// 0 = EMPTY
+		// 1 = RED (P1)
+		// 2 = BLUE (P2)
 	//Methods
 	
 	
