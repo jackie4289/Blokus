14cea18da67af9abf7fe7116660b55b6887a9f16
Lucas-M88 <lucas.moran8@gmail.com>
2022-05-27 10:23:39 -0400

    Update BlockusPanel.java

--- BlockusPanel.java
+++ BlockusPanel.java
@@ -1,13 +1,20 @@
-
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.imageio.*;
 
 public class BlockusPanel extends JPanel{
 	//Properties
 	
 	//Methods
+	public void paintComponent(Graphics g){
+		
+	}
 	
 	//Constructor
-	
-	//Main method
 	public BlockusPanel(){
 		super(); 
 	}
