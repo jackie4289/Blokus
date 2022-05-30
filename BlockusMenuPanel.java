b43281c97fb99e3096937b5d97ef0ca46a6af202
jackie4289 <jackie._lin@hotmail.com>
2022-05-30 10:28:15 -0400

    added menu Panel

---
+++ BlockusMenuPanel.java
@@ -0,0 +1,31 @@
+//
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.imageio.*;
+
+public class BlockusMenuPanel extends JPanel{
+	//Properties
+	JButton startButton = new JButton("Start");
+	JButton quitButton = new JButton("Quit");
+	
+	
+	//Methods
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		//Draw words and image
+		
+	}
+	
+	
+	
+	
+	//Constructor
+	public BlockusMenuPanel(){
+		super();
+		//import images
+	}
+}
